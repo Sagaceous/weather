0611e6f454c66da83edb33967505e3e8bb799c19
unknown <omuntaka84@gmail.com>
2020-07-12 20:02:18 +0100

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+const key = 'GmaR3BwsPQ4R3WeCPyCI9Yvs6K5YJIhn';
+
+//get weather
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch (base + query);
+    const data = await response.json();
+
+    return data[0];
+    
+};
+
+//get city here
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
